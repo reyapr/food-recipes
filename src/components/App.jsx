8faa9e366bf5fa39b0396f6b9a1a7bc147d68fcf
Renal Apriansyah <>
2019-11-16 15:26:47 +0700

    initiate css with sf pro rounded font

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import store from '../redux/config/store'
 import Card from './Card'
+import '../styles/index.css'
 
 const App = () => {
   return (
