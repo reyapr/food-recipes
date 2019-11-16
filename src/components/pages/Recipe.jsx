82a827150b9002117b92cf780dce1b1b87ccc7cd
Renal Apriansyah <>
2019-11-16 16:51:38 +0700

    change to page component

---
+++ src/components/pages/Recipe.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Card from '../organism/Card'
+import { getRecipes } from '../../redux/modules/recipe/actions'
+import { connect } from 'react-redux'
+
+const Recipe = ({ data, actionGetRecipes }) => {
+
+  useEffect(() => {
+    actionGetRecipes()
+  }, [])
+
+  return (
+    <React.Fragment>
+      <div>Home</div>
+      <Card/>
+    </React.Fragment>
+  )
+}
+
+const mapStateToProps = (state) => ({
+  recipes: state.recipes.data
+})
+
+const mapDispatchToProps = {
+  actionGetRecipes: getRecipes
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Recipe)
