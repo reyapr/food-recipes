44d293f9d1273768da91deb857fdbc8d5b5afaca
Renal Apriansyah <>
2019-11-17 00:03:23 +0700

    set layout for each card

--- src/components/organism/Card.jsx
+++ src/components/organism/Card.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import './index.css'
 
 const Card = () => {
   return (
-    <div>test</div>
+    <div className="grid-container">
+      <div className="grid-item">
+        item
+      </div>
+      <div className="grid-item">
+        item
+      </div>
+      <div className="grid-item">
+        item
+      </div>
+      <div className="grid-item">
+        item
+      </div>
+    </div>
   )
 }
 
