80886f7dddd4335731b51532282596bc141ef832
Renal Apriansyah <>
2019-11-17 10:03:41 +0700

    set layput card and image

--- src/components/organism/Card/index.jsx
+++ src/components/organism/Card/index.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import './styles.css'
 
-const Card = () => {
+const Card = ({ image, name, creator, review }) => {
   return (
-    <div>
-      item
+    <div className="card">
+      <div className="header">
+        <div style={backgRoundStyle(image)} />
+      </div>
+      <div className="card-content">
+        <div className="hidden-text-overflow name space-content">
+          {name} 
+        </div>
+        <div className="space-content">
+          By: {creator}
+        </div>
+        <div className="space-content">
+          {review}
+        </div>
+      </div>
     </div>
   )
 }
+
+const backgRoundStyle = (image) => ({
+  backgroundImage: `url(${image})`,
+  height: "125px",
+  backgroundPosition: "center",
+  backgroundSize: "cover"
+})
 
 export default Card
